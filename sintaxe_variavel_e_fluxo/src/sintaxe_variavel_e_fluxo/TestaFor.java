507549879bac53c452b85a547992cf3b87e6bad4
Douglas Vilas <douglas_vilass@hotmail.com>
2023-03-19 18:48:55 -0300

    Estrutura de Repetição FOR

---
+++ sintaxe_variavel_e_fluxo/src/sintaxe_variavel_e_fluxo/TestaFor.java
@@ -0,0 +1,10 @@
+package sintaxe_variavel_e_fluxo;
+
+public class TestaFor {
+    public static void main(String[] args) {
+
+        for (int counter = 1; counter <= 10; counter++) {
+            System.out.println(counter);
+        }
+    }
+}
